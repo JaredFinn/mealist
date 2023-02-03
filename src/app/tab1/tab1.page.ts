f41a2e5a3a0bdae9be35be30d5362210b344b2e8
Jared Finn <jaredhfinn@gmail.com>
2023-02-02 22:32:36 -0500

    carousel cards formatted

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,46 @@ export class Tab1Page {
 
   constructor() {}
 
+  recipes = [
+    {
+      title: 'Beeria Tacos',
+      calories: '',
+      fats: '',
+      sodium: '',
+      proteins: '',
+      image: '/assets/tacos.jpg'
+    },
+    {
+      title: 'Baked Ziti',
+      calories: '',
+      fats: '',
+      sodium: '',
+      proteins: '',
+      image: '/assets/baked-ziti.jpg'
+    },
+    {
+      title: 'Berry French Toast',
+      calories: '',
+      fats: '',
+      sodium: '',
+      proteins: '',
+      image: '/assets/berry-french-toast.jpg'
+    },
+    {
+      title: 'Steak & Potatoes',
+      calories: '',
+      fats: '',
+      sodium: '',
+      proteins: '',
+      image: '/assets/steak-potatoes.jpg'
+    }
+  ];
+
+  slideOpts = {
+    slidesPerView: 2,
+    loop: true,
+    centeredSlides: true,
+    freeMode: true,
+  };
+
 }
